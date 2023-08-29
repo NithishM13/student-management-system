af038b2796cc0c19295b3ba61c031728d631eb76
nithishm <nithish1326@gmail.com>
2023-08-29 22:28:51 +0530

    Load data into table

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QLabel, QWidget, \
-    QGridLayout, QLineEdit, QPushButton, QTableWidget
+    QGridLayout, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -22,13 +23,23 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile-No"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_table(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_num, row_data in enumerate(result):
+            self.table.insertRow(row_num)
+            for col_num, data in enumerate(row_data):
+                self.table.setItem(row_num, col_num, QTableWidgetItem(str(data)))
+
+        connection.close()
 
 
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.show()
+main_window = MainWindow()
+main_window.show()
+main_window.load_table()
 sys.exit(app.exec())
